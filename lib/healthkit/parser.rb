f5590cc06867080fbd44644e22c30002baace844
Howard Wilson <howard@watsonbox.net>
2017-09-22 12:26:31 +0200

    Add Healthkit::Parser.parse

--- lib/healthkit/parser.rb
+++ lib/healthkit/parser.rb
@@ -7,6 +7,24 @@ require "healthkit/parser/record"
 
 module Healthkit
   module Parser
-    # Your code goes here...
+    ELEMENT_NAMES = [:record]
+
+    def self.parse(xml, element_names: ELEMENT_NAMES)
+      element_names = element_names.map(&:to_s).map(&:camelize)
+
+      Nokogiri::XML::Reader(xml).map do |node|
+        if node.node_type == Nokogiri::XML::Reader::TYPE_DOCUMENT_TYPE
+          comment = Nokogiri::XML(node.outer_xml).at("//comment()").text.strip
+
+          if comment.start_with?("HealthKit Export Version") && comment != "HealthKit Export Version: 3"
+            raise "#{comment} is not supported"
+          end
+        end
+
+        if element_names.include?(node.name) && node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
+          "Healthkit::Parser::#{node.name.camelize}".constantize.parse(node.outer_xml)
+        end
+      end.compact
+    end
   end
 end
--- spec/healthkit/parser_spec.rb
+++ spec/healthkit/parser_spec.rb
@@ -4,4 +4,22 @@ describe Healthkit::Parser do
   it "has a version number" do
     expect(Healthkit::Parser::VERSION).not_to be nil
   end
+
+  describe ".parse" do
+    let(:xml) { File.read("spec/assets/export.xml") }
+    subject { described_class.parse(xml) }
+
+    it "correctly parses an export file" do
+      expect(subject.count).to eq 9
+      expect(subject.map(&:class).uniq).to eq [Healthkit::Parser::Record]
+    end
+
+    context "the export version is not supported" do
+      let(:xml) { File.read("spec/assets/export_unsupported_version.xml") }
+
+      it "fails" do
+        expect { subject }.to raise_exception "HealthKit Export Version: 4 is not supported"
+      end
+    end
+  end
 end
